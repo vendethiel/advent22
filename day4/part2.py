af95c979b5a9f8e469c79a3d6c1777b71a35778d
vendethiel <vendethiel@hotmail.fr>
2022-12-11 12:57:20 +0100

    day 4

---
+++ day4/part2.py
@@ -0,0 +1,29 @@
+from sys import argv
+
+def parse(lines):
+    def chunks(l):
+        data = l.split(',')
+        if len(data) != 2:
+            raise "Wrong number of segments (expected 2)"
+        return data
+    def to_range(r):
+        parts = r.split('-')
+        if len(parts) != 2:
+            raise "Wrong number of range parts (expected 2)"
+        start, end = parts
+        return range(int(start), int(end) + 1) # range is [), we want []
+
+    return map(lambda l: map(to_range, chunks(l)), lines) 
+
+def count_overlaps(data):
+    def range_contains(a, b):
+        return len(set(a) & set(b)) > 0
+
+    def either_fully_contains(a, b):
+        return range_contains(a, b) or range_contains(b, a)
+    return sum(either_fully_contains(fst, snd) for fst, snd in data)
+
+if __name__ == '__main__':
+    with open(argv[1]) as file:
+        data = file.readlines()
+        print(count_overlaps(parse(data)))
---
+++ day4/run.sh
@@ -0,0 +1,6 @@
+# TODO wrong number of `,`s
+# TODO wrong number of `-`s
+echo "# Part 1"
+python3 part1.py data.txt
+echo "# Part 2"
+python3 part2.py data.txt
